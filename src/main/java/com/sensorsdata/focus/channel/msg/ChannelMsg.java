2b6b43c190cbfd675241e684b40fe5688a33ad03
lichunjie <lichunjie@sensorsdata.cn>
2020-06-28 17:48:24 +0800

    消息体重新定义

--- src/main/java/com/sensorsdata/focus/channel/msg/ChannelMsg.java
+++ src/main/java/com/sensorsdata/focus/channel/msg/ChannelMsg.java
@@ -17,8 +17,6 @@ package com.sensorsdata.focus.channel.msg;
 
 import lombok.Data;
 
-import java.util.Map;
-
 /**
  * 神策系统中的统一通道消息
  *
@@ -27,35 +25,18 @@ import java.util.Map;
  */
 @Data
 public class ChannelMsg {
-  /** 神策系统 msgId 全局唯一 */
+  /** 神策系统 msgId 全局唯一，用途：消息发送去重用的默认 sendId */
   private String msgId;
   /** 通道返回 msgId，在调用完通道接口之后需要设置此字段 */
   private String channelMsgId;
-
-  /** 长整数，神策系统的用户唯一标识 */
+  /** 长整数，神策系统的用户唯一标识，用途：按userId去重*/
   private Long userId;
-  private Map<String, Object> userProfile;
-  /** 神策项目 id */
+  /** 神策项目 id，用途：微信 client 中如果公众号未授权，则将计划变更未暂停 */
   private Integer projectId;
-  /** 神策项目名称 */
-  private String projectName;
-  /** 消息来源信息(产品线) */
-  private MsgSource msgSource ;
-  /**
-   * 产品线来源相关的额外业务数据
-   * 例如 神策智能运营产品线含如下字段：
-   *    Integer planId - 计划 id，
-   *    String planType - 计划类型(中文名称)，
-   *    Integer planAudienceId - 受众 id，select all 的情况无值，
-   *    Integer planStrategyId - 对照组/策略组，
-   *    Integer strategyUnitId - 策略器 ID，
-   *    Long enterPlanTime - 进入计划的时间
-   */
-  private Map<String, Object> extraData;
+  /** channel ID，用途：微信 client 中如果公众号未授权，则将计划变更未暂停 */
+  private Integer channelId;
   /** 发起发送消息请求的时间，用于计算到发送完成的时间间隔或者收到消息的时间间隔 */
   private Long triggerTime;
-  /** channel ID */
-  private Integer channelId;
 
   /**
    * 通道消息体
@@ -68,10 +49,8 @@ public class ChannelMsg {
   private String failReason;
 
   /**
-   * 消息来源定义
+   * 神策扩展信息
    */
-  public enum MsgSource {
-    EXTERNAL, SA, SP, SJ, SF, SR, SRM, SPS, SM, SDG, SFO, SPE, SPT, SCA
-  }
+  private ExtraData extraData;
 
 }
