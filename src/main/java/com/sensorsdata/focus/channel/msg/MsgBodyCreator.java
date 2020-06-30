f2956820720610c85c7541b5f1e3c38befa99992
lichunjie <lichunjie@sensorsdata.cn>
2020-06-30 16:28:46 +0800

    消息体创建接口修改定义

--- src/main/java/com/sensorsdata/focus/channel/msg/MsgBodyCreator.java
+++ src/main/java/com/sensorsdata/focus/channel/msg/MsgBodyCreator.java
@@ -15,6 +15,7 @@
  */
 package com.sensorsdata.focus.channel.msg;
 
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -44,5 +45,24 @@ public abstract class MsgBodyCreator {
    * @throws java.lang.Exception 处理报错会抛出异常
    */
   public abstract String createMsgBody(String msgTemplate, Map<String, Object> propsMap) throws Exception ;
+
+  /**
+   * 根据实际消息体返回对应的神策埋点事件属性 Map；如果需要对消息体进行神策事件埋点，请在子类中实现此方法。
+   *
+   * 事件属性规范：
+   *    PUSH：$sf_msg_title, $sf_msg_content, $sf_link_url
+   *    短信：$sf_msg_content : 模板id or 短信内容
+   *    微信：$sf_msg_content : 模板id ， $sf_link_url ：H5 跳转链接
+   *    WebHook：$sf_msg_content : parameters
+   * 此接口返回的事件属性影响：只影响事件筛选，不影响业务逻辑执行
+   *    比如：已经配置好的计划中使用了 $sf_link_url ，如果没有返回这个属性的埋点值，则无法筛选出对应的结果数据，会对统计结果造成影响
+   *
+   * @param msgBody    待发送的消息体
+   * @return 神策埋点需要事件属性 Map
+   * @throws Exception 处理报错会抛出异常
+   */
+  public Map<String, Object> getEventProperties(String msgBody) throws Exception {
+    return Collections.emptyMap() ;
+  }
 
 }
