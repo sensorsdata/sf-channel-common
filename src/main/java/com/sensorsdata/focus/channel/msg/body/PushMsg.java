ca8ae4442f34f97c322ad0ee0792b9b0b5c9300f
jiazhaopu <iazhaopu@sensorsdata.cn>
2020-07-13 12:52:01 +0800

    PushMsg 删除 intentTemplate 和 activityForNativeChannel，新版本配置在 ChannelConfig

--- src/main/java/com/sensorsdata/focus/channel/msg/body/PushMsg.java
+++ src/main/java/com/sensorsdata/focus/channel/msg/body/PushMsg.java
@@ -55,16 +55,4 @@ public class PushMsg {
    */
   private Map<String, String> customized;
 
-  /**
-   * 厂商通道 intent 模板，将其中的 SF_DATA 字符串替换成 json
-   * 例如 intent:#Intent;action=com.sensors.test;component=com.sensorsdata.android.push/.TestActivity;S.test={sf_customized.news_id};S.sf_data={sf_data};end
-   * 替换成 intent:#Intent;action=com.sensors.test;component=com.sensorsdata.android.push/.TestActivity;S.test=678;S.sf_data=%7B%22customized%22%3A%7B%22book_id%22%3A%22123456%22%2C%22news_id%22%3A%22678%22%2C%22push_time%22%3A%222019-08-13+12%3A12%3A27.468%22%7D%2C%22sf_landing_type%22%3A%22CUSTOMIZED%22%2C%22sf_msg_id%22%3A%22c8f276f6-a339-4d2b-8216-c34b2f6ec586%22%2C%22sf_plan_id%22%3A%223%22%2C%22sf_audience_id%22%3A%225%22%2C%22sf_audience_entry_time%22%3A1565669547442%2C%22sf_plan_strategy_id%22%3A%224%22%7D;end
-   */
-  private String intentTemplate;
-
-  /**
-   * 有的厂商通道需要 activity 字段
-   */
-  private String activityForNativeChannel;
-
 }
